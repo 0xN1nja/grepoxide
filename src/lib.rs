b2d9ea8bac1dd7b95dbeecd2f4388308d1fa2044
Abhimanyu Sharma <71812498+0xN1nja@users.noreply.github.com>
2023-04-11 13:53:08 +0530

    Removed search_case_sensitive Function From lib.rs And Added Search Functionality In search Function

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,33 @@
 #![allow(unused)]
 #![allow(dead_code)]
 
+use colored::{self, *};
+use std::io::{self, *};
+
 pub struct Config<'a> {
     pub query: &'a str,
-    pub path: &'a str,
+    pub file_path: &'a str,
 }
 
 impl<'a> Config<'a> {
-    pub fn new(query: &'a str, path: &'a str) -> Self {
+    pub fn new(query: &'a str, file_path: &'a str) -> Self {
         Self {
             query: query,
-            path: path,
+            file_path: file_path,
         }
     }
 }
 
-pub fn search() {
-    todo!()
-}
-
-pub fn search_case_sensitive() {
-    todo!()
+pub fn search<'a>(query: &'a str, file_path: &'a str) -> Vec<String> {
+    let mut result_vec: Vec<String> = Vec::new();
+    let file: String = std::fs::read_to_string(file_path).unwrap_or_else(|_| {
+        eprintln!("Unable To Read File");
+        String::new()
+    });
+    for mut i in file.lines() {
+        if i.contains(query) {
+            result_vec.push(i.replace(query, &query.red().to_string()[..]));
+        }
+    }
+    result_vec
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 #![allow(unused)]
 #![allow(dead_code)]
 
-use colored::{self, *};
-
 fn main() {
     let args: Vec<String> = std::env::args().collect();
     if args.len() < 3 {
         eprintln!("Incorrect Number Of Arguments Supplied");
+        std::process::exit(1);
     }
     let config: grepoxide::Config = grepoxide::Config::new(&args[1][..], &args[2][..]);
+    let result_vec: Vec<String> = grepoxide::search(config.query, config.file_path);
+    for i in result_vec {
+        println!("{}", i);
+    }
 }
